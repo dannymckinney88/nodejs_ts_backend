1be939de805678142664b84f052e4fbb2c781c79
Danny <dannymckinney88@gmail.com>
2024-04-23 21:13:49 -0700

    Install bycrpt dependices, setups bycrpt, Updates tsconfig file,

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,46 @@
 import { Router } from 'express';
 import User from '../models/user.model';
 import Profile from '../models/profile.model';
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
 router.post('/register', async (req, res) => {
+    // console.log("I am the request body" ,req.body.password_hash);
+
+    const saltRounds = 10; // Number of salt rounds
+
+let hash: string; 
+
+// Hashing password
+bcrypt.hash(req.body.password_hash, saltRounds, (hashErr: Error | null, hashedPassword: string) => {
+    if (hashErr) {
+      // Handle hashing error
+      console.error('Error hashing password:', hashErr);
+    } else {
+      // Store hash in database
+      hash = hashedPassword;
+    //   console.log('Hashed password:', hash);
+  
+      // Verifying password
+      bcrypt.compare(req.body.password_hash, hash, (compareErr: Error | null, result: boolean) => {
+        if (compareErr) {
+          // Handle comparison error
+          console.error('Error comparing passwords:', compareErr);
+        } else if (result) {
+          // Passwords match
+          console.log('Passwords match');
+          req.body.password_hash = hash;
+          console.log("I am the request body" ,req.body.password_hash);
+        } else {
+          // Passwords do not match
+          console.log('Passwords do not match');
+        }
+      });
+    }
+  });
+
+    
   const {
     email,
     password_hash, // Using password_hash to match the model field
@@ -16,7 +52,10 @@ router.post('/register', async (req, res) => {
     weight,
     activity_level,
   } = req.body;
+  console.log(email, password_hash);
   
+
+
   try {
     // Create new user
     const newUser = await User.create({
